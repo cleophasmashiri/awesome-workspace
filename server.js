0796d0640fe062121d72d82cc7c223f98d832b9d
cleophas <cleophasmashiri@gmail.com>
2021-11-10 13:07:58 +0200

    express

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-/Install express server
+// Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/awesome-workspace'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/awesome-workspace/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
